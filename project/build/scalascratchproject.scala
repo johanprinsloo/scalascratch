1369006bbb026d68a4bea4fdcb4a67acadc92432
johanprinsloo <johan.a.prinsloo@gmail.com>
2011-01-02 09:35:26 -0800

    init

--- project/build/scalascratchproject.scala
+++ project/build/scalascratchproject.scala
@@ -3,4 +3,5 @@ import sbt._
 class scalascratchproject(info: ProjectInfo) extends DefaultProject(info) {
   val scalaToolsSnapshots = ScalaToolsSnapshots
   val scalatest = "org.scalatest" % "scalatest" % "1.2"
+  val scala_stm = "org.scala-tools" %% "scala-stm" % "0.2"
 }
---
+++ src/main/scala/xml/xmlscratch.scala
@@ -0,0 +1,70 @@
+package org.scratch.xml
+
+
+import java.text.SimpleDateFormat
+import java.util.Date
+import scala.xml._
+
+object xmlscratch {
+
+
+  val structXMLInAString = """
+<modelstructure>
+<graphics>
+<icon style="3D">icn504.svg</icon>
+<coords>
+       <x>654</x>
+       <y>6546</y>
+</coords>
+</graphics>
+</modelstructure>"""
+}
+
+object Config {
+  val atomPath = "atom-example.xml"
+}
+
+class Post(val title: String, val body: String, val updated: Date) {
+  lazy val dashedDate = {
+    val dashed = new SimpleDateFormat("yy-MM-dd")
+    dashed.format(updated)
+  }
+
+  lazy val atomDate = {
+    val rfc3339 = new SimpleDateFormat("yyyy-MM-dd'T'h:m:ss'-05:00'")
+    rfc3339.format(updated)
+  }
+
+  lazy val slug = title.toLowerCase.replaceAll("\\W", "-")
+  lazy val atomId  = "tag:example.com," + dashedDate + ":/" + slug
+}
+
+class AtomFeed(posts: Seq[Post]) {
+  val feed =
+    <feed xmlns="http://www.w3.org/2005/Atom">
+      <title>My Blog</title>
+      <subtitle>A fancy subtitle.</subtitle>
+        <link href="http://example.com/"/>
+        <link href="http://example.com/atom.xml" rel="self"/>
+      <updated>{posts(0).atomDate}</updated>
+      <author>
+        <name>John Doe</name>
+        <uri>http://example.com/about.html</uri>
+      </author>
+      <id>http://example.com/</id>
+      {for (post <- posts) yield
+      <entry>
+        <title>{post.title}</title>
+          <link href={"http://example.com/" + post.slug + ".html"} rel="alternate"/>
+        <id>{post.atomId}</id>
+        <updated>{post.atomDate}</updated>
+        <content type="html">{post.body}</content>
+        <author>
+          <name>John Doe</name>
+          <uri>http://example.com/about.html</uri>
+        </author>
+      </entry>}
+    </feed>
+
+  def write = XML.saveFull(Config.atomPath, feed, "UTF-8", true, null)
+}
