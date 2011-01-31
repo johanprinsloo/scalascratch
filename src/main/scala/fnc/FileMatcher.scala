e378091467a2e522751f765af08902fd0433fef7
johanprinsloo <johan.a.prinsloo@gmail.com>
2011-01-30 22:44:52 -0800

    closure over filematcher

---
+++ src/main/scala/fnc/FileMatcher.scala
@@ -0,0 +1,11 @@
+package fnc
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: johan
+ * Date: 1/30/11
+ * Time: 10:33 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+object FileMatcher
---
+++ src/main/scala/fnc/curryFn.scala
@@ -0,0 +1,11 @@
+package fnc
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: johan
+ * Date: 1/15/11
+ * Time: 7:47 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+object curryFn
