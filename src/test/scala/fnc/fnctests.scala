007cff5c0f8e40b35a6fce5444f45939ebb4111a
johanprinsloo <johan.a.prinsloo@gmail.com>
2011-02-01 20:50:51 -0800

    tests

---
+++ src/test/scala/fnc/fnctests.scala
@@ -0,0 +1,11 @@
+package fnc
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: johan
+ * Date: 2/1/11
+ * Time: 8:23 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class fnctests
---
+++ src/test/scala/stm/stmtests.scala
@@ -0,0 +1,11 @@
+package stm
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: johan
+ * Date: 2/1/11
+ * Time: 7:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class stmtests
