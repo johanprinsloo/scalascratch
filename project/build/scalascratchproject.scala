d4ce6d741e90482e484b1d5e0bea9ec68c53817f
johanprinsloo <johan.a.prinsloo@gmail.com>
2011-04-10 22:12:56 -0700

    Added snippet to illustrate the use of parallel collections with Actors.

--- project/build/scalascratchproject.scala
+++ project/build/scalascratchproject.scala
@@ -3,6 +3,6 @@ import sbt._
 class scalascratchproject(info: ProjectInfo) extends DefaultProject(info) {
   val scalaToolsSnapshots = ScalaToolsSnapshots
   val scalatest = "org.scalatest" % "scalatest" % "1.3"
-  val scala_stm = "org.scala-tools" %% "scala-stm" % "0.2"
+  val scala_stm = "org.scala-tools" % "scala-stm_2.9.0.RC1" % "0.3"
   val log4j = "log4j" % "log4j" % "1.2.13" % "compile->default"
 }
