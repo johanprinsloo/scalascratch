143fec7bb36f02a7396249809bde1486e015a28a
johanprinsloo <johan.a.prinsloo@gmail.com>
2011-05-13 18:19:18 -0700

    uuid tests

---
+++ src/main/scala/uuid/UuidMaker.scala
@@ -0,0 +1,11 @@
+package uuid
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: johan.prinsloo
+ * Date: 5/13/11
+ * Time: 4:42 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class UuidMaker
