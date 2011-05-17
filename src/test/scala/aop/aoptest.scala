ac0e1459afdd86b2a988930afd9d69fcfa35638b
Johan Prinsloo <johan.a.prinsloo@gmail.com>
2011-05-16 20:02:18 -0700

    aop

---
+++ src/test/scala/aop/aoptest.scala
@@ -0,0 +1,11 @@
+package aop
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: johan
+ * Date: 5/15/11
+ * Time: 5:49 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class aoptest
