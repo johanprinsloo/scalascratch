48dbbbb716ef2987d0ee011a090ffbdd0e3b5bc4
Johan Prinsloo <johan.a.prinsloo@gmail.com>
2011-05-17 18:47:01 -0700

    nix aop for now

--- src/test/scala/aop/aoptest.scala
+++ src/test/scala/aop/aoptest.scala
@@ -7,14 +7,14 @@ import gen.Transaction
 class aoptest extends FlatSpec with ShouldMatchers {
 
   "a functional transaction" should "be executed sequentially" in {
-    val aspect = new Aspect("execution(* *.bar(..))")
-      with InterceptorAround
-      with TransactionInterceptor
-
-    val foo = aspect.create[Foo](new FooImpl)
-
-    foo.foo("foo")
-    foo.bar("bar")
+//    val aspect = new Aspect("execution(* *.bar(..))")
+//      with InterceptorAround
+//      with TransactionInterceptor
+//
+//    val foo = aspect.create[Foo](new FooImpl)
+//
+//    foo.foo("foo")
+//    foo.bar("bar")
 
     assert(true)
 
