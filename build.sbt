af222dd31c6e6d6fc9d30508e1af42a381134a9a
Johan Prinsloo <johan.a.prinsloo@gmail.com>
2012-01-30 09:49:39 -0800

    stm test for rev 0.4

--- build.sbt
+++ build.sbt
@@ -10,7 +10,7 @@ parallelExecution in Test := false
 
 libraryDependencies ++= Seq(
     "org.scalatest" %% "scalatest" % "1.6.1",
-    "org.scala-tools" %% "scala-stm" % "0.3",
+    "org.scala-tools" %% "scala-stm" % "0.4",
     "org.scalala" % "scalala_2.9.0" % "1.0.0.RC2-SNAPSHOT",
     "log4j" % "log4j" % "1.2.13" % "compile->default",
     "ch.qos.logback" % "logback-classic" % "0.9.30",
--- src/main/scala/file/Fileplay.scala
+++ src/main/scala/file/Fileplay.scala
@@ -13,6 +13,6 @@ object Fileplay extends App {
   val holmesIterator = Source.fromFile("pg1661.txt").getLines   // holmesIterator: Iterator[String] = non-empty iterator
   holmesIterator.foreach( line => println( line ) )
 
-  val counts = mutable.Map[String, Int]().withDefault(x=>0)
-  for (token <- scala.io.Source.fromFile("pg1661.txt").getLines.flatMap(x =>x.split("\\s+"))) counts(token) +=
+  //val counts = Map[String, Int]().withDefault(x=>0)
+  //for (token <- scala.io.Source.fromFile("pg1661.txt").getLines.flatMap(x =>x.split("\\s+"))) counts(token) +=
 }
